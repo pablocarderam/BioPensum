5dae97f25b0dfe3b46eedd5b8a49622e6972100a
Pablo CR <pablocarderam@gmail.com>
2013-11-10 18:15:47 -0500

    Added correquisitos, reorganized code
    Added render() so the lines can get drawn to and from objects that
    already exist.
    Added string splitter to have more than one prerequisite or corequisite.

--- main.js
+++ main.js
@@ -7,53 +7,103 @@ ctx.scale(2,2); // scale back to full dimensions after oversampling and reductio
 
 var listaCursos = []; // base de datos todos los cursos
 var semestres = []; // array guarda arrays que son listas de cursos de cada semestre
-for (var i=0; i<8; i++) { // añade arrays para cada semestre
+for (var i=0; i<8; i++) { // añade un array dentro de semestres para cada semestre
 	var nuevoSemestre = [];
 	semestres.push(nuevoSemestre);
 }
-var biologia = false; // toggle to show classes
-var microbio = false;
+var biologia = true; // toggle to show classes TO DO: button
+var microbio = true;
 
 function cursoNuevo(curso) { // construye ícono del curso	
 	if ( (curso.programa === "-") || (biologia && curso.programa === "biologia" ) || (microbio && curso.programa === "microbiologia") ) { // Mostrar solo materias de los programas escogidos
 		semestres[curso.semestre].push(curso); // añade curso a lista de su semestre
 		curso.fila = semestres[curso.semestre].length; // guarda posición del curso en su semestre
 		curso.X = 200*curso.semestre; // ref coordinates
-		curso.Y = 50*curso.fila;
-		
-		switch (Number(curso.semestral)) {
-			case 0:
-				ctx.fillStyle = "#33AA00";
-				break;
-			case 1:
-				ctx.fillStyle = "#FF0000";
-				break;
-			case 2:
-				ctx.fillStyle = "#0044EE";
-				break;
-		}
-		if (curso.prerrequisitos !== "-") {
-			// TO DO: break prerrequisitos string into array of prerreq, compare each prerreq to each codigo in listaCursos
-			for (var i=0; i<listaCursos.length; i++) {
-				var prerreq = listaCursos[i];
-				if (curso.prerrequisitos === prerreq.codigo) {
-					console.log(prerreq.codigo);
-					ctx.lineWidth = 6;
+		curso.Y = 75*curso.fila;
+	}
+}
+
+function findPrerreq(curso) {
+	if (curso.prerrequisitos !== "-") {
+		var prerreqArr = curso.prerrequisitos.split(", "); // break prerrequisitos string into array of prerreq
+		for (var i=0; i<prerreqArr.length; i++) { // for every prerreq
+			var prerreq = prerreqArr[i];
+			for (var j=0; j<listaCursos.length; j++) { // loop through all courses
+				var otroCurso = listaCursos[j];
+				if (prerreq === otroCurso.codigo) { // compare each prerreq to each codigo in listaCursos
+					//console.log(otroCurso.codigo); 
+					ctx.lineWidth = 6;  // draw line from prerreq to current course
 					ctx.lineCap = 'round';
 					ctx.strokeStyle = "#990000";
 					ctx.beginPath();
-					ctx.moveTo(prerreq.X + 80, prerreq.Y - 3);
+					ctx.moveTo(otroCurso.X + 80, otroCurso.Y - 3);
 					ctx.lineTo(curso.X - 80, curso.Y - 3);
 					ctx.stroke();
 				}
 			}
 		}
+	}
+}
+
+function findCorreq(curso) {
+	if (curso.correquisitos !== "-") {
+		var correqArr = curso.correquisitos.split(", "); // break prerrequisitos string into array of prerreq
+		for (var i=0; i<correqArr.length; i++) { // for every prerreq
+			var correq = correqArr[i];
+			for (var j=0; j<listaCursos.length; j++) { // loop through all courses
+				var otroCurso = listaCursos[j];
+				if (correq === otroCurso.codigo) { // compare each prerreq to each codigo in listaCursos
+					//console.log(otroCurso.codigo); 
+					ctx.lineWidth = 6;  // draw line from prerreq to current course
+					ctx.lineCap = 'round';
+					ctx.strokeStyle = "#000099";
+					ctx.beginPath();
+					if (otroCurso.Y > curso.Y) {
+						ctx.moveTo(otroCurso.X, otroCurso.Y - 24);
+						ctx.lineTo(curso.X, curso.Y + 18);
+					}
+					else {
+						ctx.moveTo(otroCurso.X, otroCurso.Y + 18);
+						ctx.lineTo(curso.X, curso.Y - 24);
+					}
+					ctx.stroke();
+				}
+			}
+		}
+	}
+}
+
+function render() {
+	for (var i=0; i<listaCursos.length; i++) {
+		var curso = listaCursos[i];
+		
+		switch (Number(curso.semestral)) { // Different colors show primer semestre, segundo semestre, or anual
+			case 0:
+				ctx.fillStyle = "#33AA00"; // anual is green
+				break;
+			case 1:
+				ctx.fillStyle = "#FF0000"; // semestre 1 is red
+				break;
+			case 2:
+				ctx.fillStyle = "#0044EE"; // semestre 2 is blue
+				break;
+		}
+		
+		findPrerreq(curso); // draw prerrequisito lines
+		findCorreq(curso); // draw correquisito lines
 		ctx.fillRect(curso.X-75, curso.Y-18, 150, 30); // dibuja rectangulo
 		ctx.fillStyle = '#000000';
 		ctx.textAlign = 'center';
 		ctx.font = 'bold 10pt Arial';
 		ctx.fillText(curso.nombre, curso.X, curso.Y); // escribe nombre
 	}
+}
+
+function init() {
+	for (var i=0; i<listaCursos.length; i++) { // crea cada curso en listaCursos
+		cursoNuevo(listaCursos[i]);
+	}
+	render(); // render all cursos and lines
 }
 
 function preload() { // load db before init
@@ -65,14 +115,8 @@ function preload() { // load db before init
 	
 	function showInfo(data, tabletop) {
 		listaCursos = data; // save spreadsheet to js
-		console.log(listaCursos);
+		//console.log(listaCursos);
 		init();
-	}
-}
-
-function init() {
-	for (var i=0; i<listaCursos.length; i++) { // crea cada curso en listaCursos
-		cursoNuevo(listaCursos[i]);
 	}
 }
 
