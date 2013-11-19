20768980d5c5144e28eba70986fc1f93c9e87941
Pablo CR <pablocarderam@gmail.com>
2013-11-18 20:42:18 -0500

    Bunch of stuff
    Added semestres and creditos counters.
    Spreadhseet opens in a new tab.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// JavaScript Document
+// JavaScript Document TO DO: DRAG COURSES
 
 var page = document.getElementById("page"), // get div element covering whole page, for mouse and touch events
 stage = document.getElementById("stage"), // get canvas element and context
@@ -7,12 +7,12 @@ ctx.scale(2, 2); // scale back to full dimensions after oversampling and reducti
 
 var listaCursos = []; // base de datos todos los cursos
 var semestres = []; // array guarda arrays que son listas de cursos de cada semestre
-for (var i=0; i<8; i++) { // añade un array dentro de semestres para cada semestre
-	var nuevoSemestre = [];
-	semestres.push(nuevoSemestre);
-}
-var biologia = true; // toggle to show classes TO DO: button
+
+var biologia = true; // toggle to show classes
 var microbio = true;
+
+var btnBiologia = { X : 15, Y : 20, hitRadius : 15 }; // botones de programa, toggle on-off
+var btnMicrobio = { X : 15, Y : 40, hitRadius : 15 };
 
 // Appearance
 var boxWidth = 150;
@@ -21,13 +21,19 @@ var spacerX = 50;
 var spacerY = 30;
 var lineWidth = 6;
 
-
-function cursoNuevo(curso) { // construye ícono del curso	
-	if ( (curso.programa === "-") || (biologia && curso.programa === "biologia" ) || (microbio && curso.programa === "microbiologia") ) { // Mostrar solo materias de los programas escogidos
+function cursoNuevo(curso) { // construye ícono del curso
+	if ( (curso.programa === "-") || (biologia && (curso.programa === "biologia") ) || (microbio && curso.programa === "microbiologia" ) ) { // Mostrar solo materias de los programas escogidos
+		curso.show = true;
 		semestres[curso.semestre].push(curso); // añade curso a lista de su semestre
+		semestres[curso.semestre].creditos = (Number(semestres[curso.semestre].creditos) + Number(curso.creditos)); // añadir creditos
 		curso.fila = semestres[curso.semestre].length; // guarda posición del curso en su semestre
 		curso.X = (boxWidth+spacerX)*(curso.semestre-0.5); // ref coordinates
-		curso.Y = (boxHeight+spacerY)*(curso.fila-0.5);
+		curso.Y = (boxHeight+spacerY)*(curso.fila+1.5);
+		curso.hitRadius = 15;
+	}
+	else {
+		//console.log(curso.codigo);
+		curso.show = false;
 	}
 }
 
@@ -39,28 +45,32 @@ function findPrerreq(curso) {
 			for (var j=0; j<listaCursos.length; j++) { // loop through all courses
 				var otroCurso = listaCursos[j];
 				if (prerreq === otroCurso.codigo) { // compare each prerreq to each codigo in listaCursos
-					console.log(otroCurso.codigo); 
-					ctx.lineWidth = lineWidth;  // draw line from prerreq to current course
-					ctx.lineCap = 'round';
-					ctx.lineJoin = 'round';
-					ctx.strokeStyle = "#990000";
-					ctx.beginPath();
-					var dif = Math.abs(otroCurso.semestre-curso.semestre);
-					if (dif === 0) { // si estan en el mismo semestre, alertar
-						alert (otroCurso.nombre + " es prerrequisito de " + curso.nombre + " y no pueden estar en el mismo semestre.");
+					if (otroCurso.show) {
+						ctx.lineWidth = lineWidth;  // draw line from prerreq to current course
+						ctx.lineCap = 'round';
+						ctx.lineJoin = 'round';
+						ctx.strokeStyle = "#990000";
+						ctx.beginPath();
+						var dif = Math.abs(otroCurso.semestre-curso.semestre);
+						if (dif === 0) { // si estan en el mismo semestre, alertar
+							alert (otroCurso.nombre + " es prerrequisito de " + curso.nombre + " y no pueden estar en el mismo semestre.");
+						}
+						else if (dif === 1) { // si estan a un semestre de distancia, dibujar linea normal
+							ctx.moveTo(otroCurso.X + boxWidth/2 + lineWidth, otroCurso.Y);
+							ctx.lineTo(curso.X - boxWidth/2 - lineWidth, curso.Y);
+						}
+						else { // si están a más de un semestre de distancia, 
+							ctx.moveTo(otroCurso.X + boxWidth/2 + lineWidth, otroCurso.Y);
+							ctx.lineTo(otroCurso.X + boxWidth/2 + lineWidth, otroCurso.Y + (boxHeight+spacerY)/2); // dibujar línea hacia a bajo,
+							ctx.lineTo(curso.X - boxWidth/2 - spacerX, otroCurso.Y + (boxHeight+spacerY)/2); // luego linea que pase entre los otros cursos,
+							ctx.lineTo(curso.X - boxWidth/2 - lineWidth, curso.Y); // luego linea normal
+						}
+						
+						ctx.stroke();
 					}
-					else if (dif === 1) { // si estan a un semestre de distancia, dibujar linea normal
-						ctx.moveTo(otroCurso.X + boxWidth/2 + lineWidth, otroCurso.Y);
-						ctx.lineTo(curso.X - boxWidth/2 - lineWidth, curso.Y);
+					else {
+						alert("El curso " + curso.codigo + " tiene como prerrequisito al curso " + otroCurso.codigo);
 					}
-					else { // si están a más de un semestre de distancia, 
-						ctx.moveTo(otroCurso.X + boxWidth/2 + lineWidth, otroCurso.Y);
-						ctx.lineTo(otroCurso.X + boxWidth/2 + lineWidth, otroCurso.Y + (boxHeight+spacerY)/2); // dibujar línea hacia a bajo,
-						ctx.lineTo(curso.X - boxWidth/2 - spacerX, otroCurso.Y + (boxHeight+spacerY)/2); // luego linea que pase entre los otros cursos,
-						ctx.lineTo(curso.X - boxWidth/2 - lineWidth, curso.Y); // luego linea normal
-					}
-					
-					ctx.stroke();
 				}
 			}
 		}
@@ -75,20 +85,24 @@ function findCorreq(curso) {
 			for (var j=0; j<listaCursos.length; j++) { // loop through all courses
 				var otroCurso = listaCursos[j];
 				if (correq === otroCurso.codigo) { // compare each prerreq to each codigo in listaCursos
-					//console.log(otroCurso.codigo); 
-					ctx.lineWidth = lineWidth;  // draw line from prerreq to current course
-					ctx.lineCap = 'round';
-					ctx.strokeStyle = "#000099";
-					ctx.beginPath();
-					if (otroCurso.Y > curso.Y) {
-						ctx.moveTo(otroCurso.X, otroCurso.Y - boxHeight/2 - lineWidth);
-						ctx.lineTo(curso.X, curso.Y + boxHeight/2 + lineWidth);
+					if (otroCurso.show) { // if correquisito is displayed
+						ctx.lineWidth = lineWidth;  // draw line from prerreq to current course
+						ctx.lineCap = 'round';
+						ctx.strokeStyle = "#000099";
+						ctx.beginPath();
+						if (otroCurso.Y > curso.Y) {
+							ctx.moveTo(otroCurso.X, otroCurso.Y - boxHeight/2 - lineWidth);
+							ctx.lineTo(curso.X, curso.Y + boxHeight/2 + lineWidth);
+						}
+						else {
+							ctx.moveTo(otroCurso.X, otroCurso.Y + boxHeight/2 + lineWidth);
+							ctx.lineTo(curso.X, curso.Y - boxHeight/2 - lineWidth);
+						}
+						ctx.stroke();
 					}
-					else {
-						ctx.moveTo(otroCurso.X, otroCurso.Y + boxHeight/2 + lineWidth);
-						ctx.lineTo(curso.X, curso.Y - boxHeight/2 - lineWidth);
+					else { // si no se muestra el correquisito, avisar
+						alert("El curso " + curso.codigo + " tiene como correquisito al curso " + otroCurso.codigo);
 					}
-					ctx.stroke();
 				}
 			}
 		}
@@ -96,34 +110,109 @@ function findCorreq(curso) {
 }
 
 function render() {
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fillRect(0, 0, stage.width, stage.height);
 	for (var i=0; i<listaCursos.length; i++) {
 		var curso = listaCursos[i];
-		
-		switch (Number(curso.semestral)) { // Different colors show primer semestre, segundo semestre, or anual
-			case 0:
-				ctx.fillStyle = "#33AA00"; // anual is green
-				break;
-			case 1:
-				ctx.fillStyle = "#FF0000"; // semestre 1 is red
-				break;
-			case 2:
-				ctx.fillStyle = "#0044EE"; // semestre 2 is blue
-				break;
+		if (curso.show) {
+			
+			switch (Number(curso.semestral)) { // Different colors show primer semestre, segundo semestre, or anual
+				case 0:
+					ctx.fillStyle = "#33AA00"; // anual is green
+					break;
+				case 1:
+					ctx.fillStyle = "#FF0000"; // semestre 1 is red
+					break;
+				case 2:
+					ctx.fillStyle = "#0044EE"; // semestre 2 is blue
+					break;
+			}
+			
+			findPrerreq(curso); // draw prerrequisito lines
+			findCorreq(curso); // draw correquisito lines
+			ctx.fillRect(curso.X-boxWidth/2, curso.Y-boxHeight/2, boxWidth, boxHeight); // dibuja rectangulo
+			ctx.fillStyle = '#000000';
+			ctx.textAlign = 'center';
+			ctx.font = 'bold 10pt Arial';
+			ctx.fillText(curso.nombre, curso.X, curso.Y+lineWidth); // escribe nombre
 		}
-		
-		findPrerreq(curso); // draw prerrequisito lines
-		findCorreq(curso); // draw correquisito lines
-		ctx.fillRect(curso.X-boxWidth/2, curso.Y-boxHeight/2, boxWidth, boxHeight); // dibuja rectangulo
-		ctx.fillStyle = '#000000';
-		ctx.textAlign = 'center';
-		ctx.font = 'bold 10pt Arial';
-		ctx.fillText(curso.nombre, curso.X, curso.Y+lineWidth); // escribe nombre
+	}
+	
+	// dibuja botones de toggle biologia y microbiologia
+	//ctx.fillRect(btnBiologia.X-10, btnBiologia.Y-10, 20, 20); // dibuja rectangulo
+	ctx.fillStyle = '#000000';
+	ctx.textAlign = 'left';
+	ctx.font = 'bold 12pt Arial';
+	if (biologia) {
+		ctx.fillText("Quitar cursos biologia", btnBiologia.X, btnBiologia.Y+lineWidth); // escribe nombre
+	}
+	else {
+		ctx.fillText("Mostrar cursos biologia", btnBiologia.X, btnBiologia.Y+lineWidth); // escribe nombre
+	}
+	
+	if (microbio) {
+		ctx.fillText("Quitar cursos microbiologia", btnMicrobio.X, btnMicrobio.Y+lineWidth); // escribe nombre
+	}
+	else {
+		ctx.fillText("Mostrar cursos microbiologia", btnMicrobio.X, btnMicrobio.Y+lineWidth); // escribe nombre
+	}
+	
+	// dibujar semestres y créditos
+	ctx.textAlign = 'center';
+	ctx.font = '11pt Arial';
+	for (var j=0; j<semestres.length; j++) { // añade un array dentro de semestres para cada semestre
+		ctx.fillText("Semestre " + (j+1), (boxWidth+spacerX)*(j+0.5), (boxHeight+spacerY*2)+lineWidth); // escribe semestre
+		ctx.fillText("Créditos: " + semestres[(j+1)].creditos, (boxWidth+spacerX)*(j+0.5), (boxHeight+spacerY*2.5)+lineWidth); // escribe semestre
 	}
 }
 
-function init() {
-	for (var i=0; i<listaCursos.length; i++) { // crea cada curso en listaCursos
-		cursoNuevo(listaCursos[i]);
+function hitTest(obj, x, y) {  // tests against stage coordinates
+	if (getDistance(obj.X, obj.Y, x, y) < obj.hitRadius) { 
+		return true;
+	} 
+	else { 
+		return false;
+	}
+}
+
+function getDistance(x1, y1, x2, y2) { // find distance between two points
+	var dist = Math.round(Math.sqrt(Math.pow((x1 - x2)/5.5, 2) + Math.pow(y1 - y2, 2))); //altered x coordinates to make buttons wide
+	return dist;
+}
+
+function clickHandler(e) { // click 
+	var rect = stage.getBoundingClientRect();
+	var clickX = e.clientX - rect.left; // find real coord
+	var clickY = e.clientY - rect.top;
+	if (hitTest(btnBiologia, clickX-80, clickY)) {
+		biologia = !biologia; // toggle biologia courses
+		//alert("toggle bio");
+		init();
+	}
+	else if (hitTest(btnMicrobio, clickX-80, clickY)) {
+		microbio = !microbio; // toggle microbiologia courses
+		//alert("toggle microbio");
+		init();
+	}
+	else { // display course info
+		for (var i=0; i<listaCursos.length; i++) {
+			if (hitTest(listaCursos[i], clickX, clickY)) {
+				alert("Nombre: " + listaCursos[i].nombre + ". Código: " + listaCursos[i].codigo);
+			}
+		}
+	}
+}
+
+function init() { // re draw everything
+	semestres = [];
+	
+	for (var i=0; i<8; i++) { // añade un array dentro de semestres para cada semestre
+		var nuevoSemestre = [];
+		nuevoSemestre.creditos = 0;
+		semestres.push(nuevoSemestre);
+	}
+	for (var j=0; j<listaCursos.length; j++) { // crea cada curso en listaCursos
+		cursoNuevo(listaCursos[j]);
 	}
 	render(); // render all cursos and lines
 }
@@ -137,15 +226,15 @@ function preload() { // load db before init
 	
 	function showInfo(data, tabletop) {
 		listaCursos = data; // save spreadsheet to js
-		//console.log(listaCursos);
 		if (listaCursos.length > 1) {
 			init();
-			//alert(listaCursos[1].nombre);
 		}
 		else {
 			alert("Error: no pude encontrar la lista de cursos");
 		}
 	}
+	
+	stage.addEventListener("click", clickHandler, false); // click handler
 }
 
 preload();
